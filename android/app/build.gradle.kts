77bddb1dea98036e9e9096cc63fceabc118e9853
adel <meriouaadel22@gmail.com>
2025-03-30 09:25:41 -0700

    change name and icon of the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.master2.coffee_ordering"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
